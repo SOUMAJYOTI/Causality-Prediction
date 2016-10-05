Eta average fixed

src/09_24/temporal_logic_test.py
@@ -31,6 +31,12 @@ class TLogic:
         self.measures = measures
         self.cascade_df['time_date']= pd.to_datetime(self.cascade_df['time_date'], format='%Y-%m-%d %H:%M:%S')
         self.cascade_df = self.cascade_df.reset_index(drop=True)
+        # print(self.cascade_df)
+        self.dnIntervals_cause = {}
+        self.dnIntervals_effect = {}
+        for idx in range(len(self.measures)):
+            self.dnIntervals_effect[self.measures[idx]] = []
+            self.dnIntervals_cause[self.measures[idx]] = []
 
     def dynamic_intervals(self, start, delta_t):
         mean_series = np.mean(self.cascade_df[self.measures[0]])
@@ -51,70 +57,83 @@ class TLogic:
 
     def rules_formulas(self, delta_t, lag):
         time_points = self.cascade_df['time_date'].tolist()
-        mean_measure_value = np.mean(self.cascade_df[self.measures[0]]) / 2
-        startPoint = time_points[0]
-        endPoint = startPoint + datetime.timedelta(minutes=delta_t)
-        start_mark = 0
-        start_idx = 0
-        self.dnIntervals_cause = []
-        self.dnIntervals_effect = []
-        while endPoint < time_points[len(time_points)-1]:
-            # check for statisfaction of formula 1
-            while startPoint < endPoint and endPoint < time_points[len(time_points)-1]:
-                if self.cascade_df[self.measures[0]][start_idx] < mean_measure_value:
-                    if start_idx+1 >= len(time_points):
+        for idx_measures in range(len(self.measures)):
+            # print(self.measures[idx_measures])
+            mean_measure_value = np.mean(self.cascade_df[self.measures[idx_measures]]) / 3
+            startPoint = time_points[0]
+            endPoint = startPoint + datetime.timedelta(minutes=delta_t)
+            start_mark = 0
+            start_idx = 0
+
+            while endPoint < time_points[len(time_points)-1]:
+                # check for statisfaction of formula 1
+                while startPoint < endPoint and endPoint < time_points[len(time_points)-1]:
+                    # print(self.cascade_df[self.measures[idx_measures]][start_idx], mean_measure_value)
+                    if self.cascade_df[self.measures[idx_measures]][start_idx] < mean_measure_value:
+                        if start_idx+1 >= len(time_points):
+                            break
+                        startPoint = time_points[start_idx+1]
+                        endPoint = startPoint + datetime.timedelta(minutes=delta_t)
                         break
-                    startPoint = time_points[start_idx+1]
-                    endPoint = startPoint + datetime.timedelta(minutes=delta_t)
-                    break
-                start_idx += 1
-                if start_idx >= len(time_points):
-                    break
-                startPoint = time_points[start_idx]
+                    start_idx += 1
+                    if start_idx >= len(time_points):
+                        break
+                    startPoint = time_points[start_idx]
 
-            # check for satisfaction of formula 2
-            if startPoint >= endPoint:
-                start_date = time_points[start_mark] + datetime.timedelta(minutes=lag) - datetime.timedelta(minutes=delta_t)
-                start_date = pd.to_datetime(start_date)
-                end_date = pd.to_datetime(time_points[start_mark] + datetime.timedelta(minutes=lag))
+                # check for satisfaction of formula 2
+                if startPoint >= endPoint:
+                    start_date = time_points[start_mark] + datetime.timedelta(minutes=lag) - datetime.timedelta(minutes=delta_t)
+                    start_date = pd.to_datetime(start_date)
+                    end_date = pd.to_datetime(time_points[start_mark] + datetime.timedelta(minutes=lag))
 
-                # try:
-                num_shares_first = self.cascade_df[self.cascade_df['time_date'] >= start_date]
-                num_shares_first = num_shares_first[num_shares_first['time_date'] < end_date]
+                    # try:
+                    num_shares_first = self.cascade_df[self.cascade_df['time_date'] >= start_date]
+                    num_shares_first = num_shares_first[num_shares_first['time_date'] < end_date]
 
-                start_date = time_points[start_mark] + datetime.timedelta(minutes=lag)
-                start_date = pd.to_datetime(start_date)
-                end_date = pd.to_datetime(
-                    time_points[start_mark] + datetime.timedelta(minutes=lag) + datetime.timedelta(minutes=delta_t),
-                    format='%Y-%m-%d %H:%M:%S')
-
-                try:
+                    start_date = time_points[start_mark] + datetime.timedelta(minutes=lag)
+                    start_date = pd.to_datetime(start_date)
+                    end_date = pd.to_datetime(
+                        time_points[start_mark] + datetime.timedelta(minutes=lag) + datetime.timedelta(minutes=delta_t),
+                        format='%Y-%m-%d %H:%M:%S')
                     num_shares_second = self.cascade_df[self.cascade_df['time_date'] >= start_date]
                     num_shares_second = num_shares_second[num_shares_second['time_date'] < end_date]
-                    print(len(num_shares_first), len(num_shares_second))
-                    if len(num_shares_first) / len(num_shares_second) >= 6:
-
-                        e_start = num_shares_second.index
-                        e_end = num_shares_second.index[len(num_shares_second) - 1]
-
-                        print(e_start, e_end)
-                        self.dnIntervals_cause.append((start_mark, start_idx))
-                        self.dnIntervals_effect.append((e_start, e_end))
+                    try:
                         # print(len(num_shares_first), len(num_shares_second))
-                except:
-                    pass
+                        if len(num_shares_first) / len(num_shares_second) >= 3:
+
+                            e_start = num_shares_second.index[0]
+                            e_end = num_shares_second.index[len(num_shares_second) - 1]
+
+                            self.dnIntervals_cause[self.measures[idx_measures]].append((start_mark, start_idx))
+                            self.dnIntervals_effect[self.measures[idx_measures]].append((e_start, e_end))
+                            # print(len(num_shares_first), len(num_shares_second))
+                    except:
+                        pass
 
 
-                startPoint = endPoint
-                endPoint += datetime.timedelta(minutes=delta_t)
-            start_idx += 1
-            start_mark = start_idx
-            if start_idx >= len (time_points):
-                break
-        print(self.dnIntervals_cause)
-        print(self.dnIntervals_effect)
+                    startPoint = endPoint
+                    endPoint += datetime.timedelta(minutes=delta_t)
+                start_idx += 1
+                start_mark = start_idx
+                if start_idx >= len (time_points):
+                    break
+        # print(self.dnIntervals_cause)
+        # print(self.dnIntervals_effect)
 
-    # def eta_avg(self):
+    def eta_avg(self):
+        for idx_measures in range(len(self.measures)):
+            cause_prime = self.dnIntervals_cause[self.measures[idx_measures]]
+            effect_prime = self.dnIntervals_effect[self.measures[idx_measures]]
+            for idx_cause in range(len(cause_prime)):
+                for idx_nest in range(len(self.measures)):
+                    if idx_nest == idx_measures:
+                        continue
+                    cause_second = self.dnIntervals_cause[self.measures[idx_nest]]
+                    effect_second = self.dnIntervals_effect[self.measures[idx_nest]]
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -150,7 +169,7 @@ if __name__ == '__main__':
     critical_values = {}
     granger_cause_count = {}
 
-    for L in range(0, 2):
+    for L in range(len(measures), len(range(len(measures))) + 1):
         for subset in itertools.combinations(range(len(measures)), L):
             num_measures = len(subset)
             if num_measures == 0:
@@ -171,6 +190,7 @@ if __name__ == '__main__':
                     # if measures[subset[idx]] == 'bw' or measures[subset[idx]] == 'cc':
                     cascade_df_feat[measures[subset[idx]]] = measure_time_df[subset[idx]][mid][measures[subset[idx]]]
                 cascade_df_feat = cascade_df_feat.sort('time')
+                cascade_df_feat = cascade_df_feat.dropna()
                 #cascade_df_feat = cascade_df_feat[cascade_df_feat['time'] < pd.to_datetime(inhib_time)]
 
                 # print(cascade_df_eat)
@@ -218,7 +238,6 @@ if __name__ == '__main__':
                     Y.append(Y_act[idx])
                     time_new.append(time_series[idx])
 
-
                 cascade_ts_df = pd.DataFrame()
                 cascade_ts_df['time_diff'] = Y
                 cascade_ts_df['time_date']= time_new
@@ -228,6 +247,30 @@ if __name__ == '__main__':
                 s = cascade_ts_df.shape
                 cascade_ts_df = cascade_ts_df.fillna(0)
 
+                # for idx_sub in range(len(subset)):
+                #     measures_causality.append(measures[subset[idx_sub]])
+                #     measures_string += (measures[subset[idx_sub]] + ' + ')
+                # measures_string = measures_string[:len(measures_string) - 3]
+
+                Y_diff = []
+                X_diff = [[] for i in range(len(subset))]
+                for idx in range(len(Y)):
+                    if np.isnan(Y[idx]):
+                        continue
+                    Y_diff.append(Y[idx])
+                    for idx_sub in range(len(subset)):
+                        X_diff[idx_sub].append(X[idx_sub][idx])
+
+                X = np.asarray(X_diff)
+                Y = np.asarray(Y_diff)
+
+                cascade_VAR_df = pd.DataFrame()
+                cascade_VAR_df['time_diff'] = Y
+                for idx_sub in range(len(subset)):
+                    # print(measures[subset[idx_sub]])
+                    cascade_VAR_df[measures[subset[idx_sub]]] = X[idx_sub, :]
+
+                # print(cascade_VAR_df)
                 # print(cascade_ts_df)
                 # print(cascade_ts_df.shape)
                 # Step 3: Remove the NaN values from the df.
@@ -259,9 +302,12 @@ if __name__ == '__main__':
 
                 # plt.plot(range(len(cascade_ts_df.index)), cascade_ts_df['pr_ratios'])
                 # plt.show()
-                temporal_logic = TLogic(cascade_ts_df, measures)
-                temporal_logic.rules_formulas(200, 500)
 
                 cnt_mids += 1
-                if cnt_mids > 0:
+                temporal_logic = TLogic(cascade_ts_df, measures)
+                temporal_logic.rules_formulas(50, 150)
+                temporal_logic.eta_avg()
+                print('Mid: ', cnt_mids)
+                if cnt_mids > 2:
                     break
+